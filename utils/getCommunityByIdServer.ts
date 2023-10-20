a462b19b17f1894fee6dc78ab6f67d9ae0c2bc90
Yong Kim <yhkim4573@gmail.com>
2023-10-20 14:48:48 -0400

    env update

--- utils/getCommunityByIdServer.ts
+++ utils/getCommunityByIdServer.ts
@@ -1,7 +1,10 @@
 export const getCommunityByIdServer = async (id: string) => {
-  const res = await fetch(`${process.env.VERCEL_URL}/api/community/${id}`, {
-    cache: process.env.CACHE_TYPE as RequestCache,
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/community/${id}`,
+    {
+      cache: process.env.CACHE_TYPE as RequestCache,
+    }
+  );
   const {
     communities,
     posts,
