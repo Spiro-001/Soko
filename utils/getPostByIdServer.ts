787a1023c3a22de82f7958f5e085bbcf14c1f3fb
Yong Kim <yhkim4573@gmail.com>
2023-10-20 14:21:08 -0400

    fetch url changed to vercel

--- utils/getPostByIdServer.ts
+++ utils/getPostByIdServer.ts
@@ -1,10 +1,7 @@
 export const getPostByIdServer = async (id: string) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/post/${id}`,
-    {
-      cache: "no-store",
-    }
-  );
+  const res = await fetch(`${process.env.VERCEL_URL}/api/post/${id}`, {
+    cache: "no-store",
+  });
   const post: PostByIdType = await res.json();
   return post;
 };
--- utils/getUserByIdServer.ts
+++ utils/getUserByIdServer.ts
@@ -1,10 +1,7 @@
 export const getUserByIdServer = async (id: string) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/${id}`,
-    {
-      cache: process.env.CACHE_TYPE as RequestCache,
-    }
-  );
+  const res = await fetch(`${process.env.VERCEL_URL}/api/user/${id}`, {
+    cache: process.env.CACHE_TYPE as RequestCache,
+  });
   const user: UserType = await res.json();
   return user;
 };
