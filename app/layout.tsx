b02081ffd27133517a9a1a0bac66cd359f7bca16
Yong Kim <yhkim4573@gmail.com>
2023-10-20 14:46:21 -0400

    force dynamic layout app

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
   title: "Soko",
