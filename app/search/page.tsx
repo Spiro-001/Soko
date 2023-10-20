dbce33e1cf0cf4715722acf22e194a6329a5fed6
Yong Kim <yhkim4573@gmail.com>
2023-10-20 01:31:08 -0400

    Fixed typescript error for build

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import CommunityModal from "@/components/CommunityModal";
 import ProvidePostClient from "@/components/ProvidePostClient";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/nextAuth";
 
 const Search = async () => {
+  const session = (await getServerSession(authOptions)) as Session;
   return (
     <>
       <div className="border border-black row-start-1 row-end-7 col-start-1 col-end-2 flex-col items-center pt-8 pb-16 gap-4 xl:flex hidden">
         <div className="sticky top-56"></div>
       </div>
       <div className="border border-black row-start-1 row-end-7 flex flex-col items-center pt-8 pb-16 px-4 gap-4 col-start-2 col-end-3">
-        <ProvidePostClient />
+        <ProvidePostClient session={session} />
       </div>
       <div className="border border-black row-start-1 row-end-7 col-start-3 col-end-4 flex-col items-center pt-8 pb-16 px-4 gap-4 xl:flex hidden">
         <div className="top-56 w-full sticky">
--- components/ProvidePostClient.tsx
+++ components/ProvidePostClient.tsx
@@ -5,7 +5,7 @@ import { useSearchParams } from "next/navigation";
 import { getPostClient } from "@/utils/getPostClient";
 import Feed from "./Feed";
 
-const ProvidePostClient = () => {
+const ProvidePostClient = ({ session }: { session: Session | null }) => {
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
   const [posts, setPosts] = useState<PostType[] | []>([]);
@@ -18,7 +18,7 @@ const ProvidePostClient = () => {
     requestPost();
   }, [query]);
 
-  return <Feed posts={posts} />;
+  return <Feed posts={posts} session={session} />;
 };
 
 export default ProvidePostClient;
