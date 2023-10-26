57fa21692230f2b633baf3ea795fbc915f83e1f0
Yong Kim <yhkim4573@gmail.com>
2023-10-26 13:54:32 -0400

    production Link navigation bugs

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,15 +19,16 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
         </main>
       </Provider>
     );
-  } else {
-    return (
-      <main className="min-h-screen flex flex-col bg-neutral-100">
-        <div className="flex-1 grid grid-flow-row grid-rows-6 grid-cols-[minmax(0,_1fr)_minmax(min-content,_2fr)_minmax(0,_1fr)]">
-          <Login />
-        </div>
-      </main>
-    );
   }
+  // else {
+  //   return (
+  //     <main className="min-h-screen flex flex-col bg-neutral-100">
+  //       <div className="flex-1 grid grid-flow-row grid-rows-6 grid-cols-[minmax(0,_1fr)_minmax(min-content,_2fr)_minmax(0,_1fr)]">
+  //         <Login />
+  //       </div>
+  //     </main>
+  //   );
+  // }
 };
 
 export default Layout;
